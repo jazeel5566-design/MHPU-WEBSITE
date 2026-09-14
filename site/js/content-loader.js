a50bd52dcb39c7520401ecbbfa25283fe0497a01
jazeel5566-design <jazeel5566@gmail.com>
2026-09-15 01:07:38 +0500

    Add files via upload

--- site/js/content-loader.js
+++ site/js/content-loader.js
@@ -519,7 +519,7 @@
     function startAutoRotate() {
       if (autoTimer) clearInterval(autoTimer);
       if (urls.length > 1) {
-        autoTimer = setInterval(function () { setImage(i + 1); }, 30000);
+        autoTimer = setInterval(function () { setImage(i + 1); }, 5000);
       }
     }
 
